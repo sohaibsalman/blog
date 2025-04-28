0813c659e03467c4c339d53402a79865466137b5
sohaibsalman <sohaibsalman10@gmail.com>
2025-04-29 02:04:38 +0500

    Add projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,26 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Online Education Platform ',
+    description: `A comprehensive Online Education platform, to streamline and enhance communication, learning, 
+      and administration in educational institutions. Developed using .NET Core, React, SignalR, SQL Server, Entity Framework.`,
+    imgSrc: '/static/images/online-education-platform.png',
+    href: '/projects',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Web based Tailor System',
+    description: `Created an advanced online tailoring system designed to streamline and expedite manual tasks 
+      for a clothing tailor brand/shop using ASP.NET Web Forms and HTML, CSS, JavaScript and jQuery.`,
+    imgSrc: '/static/images/tailor-system.png',
+    href: '/projects',
+  },
+  {
+    title: 'Right Page Chrome Extension',
+    description: `Rightpage is a powerful Chrome Extension designed to streamline your customer support by generating 
+    automated responses from Zendesk tickets. It allows users to tailor their responses with a personalized touch by 
+    setting default draft templates for the messages. Developed using React, Ant Design, Zustand`,
+    imgSrc: '/static/images/rightpage.png',
+    href: '/projects',
   },
 ]
 
